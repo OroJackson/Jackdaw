acdf20cee9b31d1712b07756d4c75fdcd9d17736
AurelieDigeon <aurelie.digeon@gmail.com>
2014-01-11 15:20:08 +0100

    changement de place de Covoiturage pour import

--- src/membre/Covoiturage.java
+++ src/membre/Covoiturage.java
@@ -1,8 +1,10 @@
-package covoiturage;
+package membre;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import vehicules.Voiture;
 
 public class Covoiturage {
 	private List<Passager> membres = new ArrayList<Passager>();
