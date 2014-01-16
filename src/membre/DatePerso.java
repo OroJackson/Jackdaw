41c91dc7a1702dd58fba4481d7333553400ec8b5
Dashell <galasxalain@gmail.com>
2014-01-16 20:51:43 +0100

    REPRISE COMPLETE des parse exeption, verification date et heure operationel, suppression de l'atribut heure de trajet (utilisation d'un seul calendar) correction du double ajout des trajets de la fonction add ON EST DES BG

--- src/membre/DatePerso.java
+++ src/membre/DatePerso.java
@@ -5,6 +5,13 @@ import java.util.*;
 
 public class DatePerso extends GregorianCalendar{
 
+	public DatePerso(){}
+	
+	public DatePerso(String dateT) throws ParseException{
+		SimpleDateFormat format =new SimpleDateFormat ("dd:MM:yyyy");
+		Date date = format.parse(dateT);
+		setTime(date);
+	}
 	 
 	/**
 	 * 
@@ -12,16 +19,11 @@ public class DatePerso extends GregorianCalendar{
 	private static final long serialVersionUID = 1L;
 
 	
-	public boolean toDate(String dateT) throws ParseException{
+	public 	void toDate(String dateT) throws ParseException{
 		SimpleDateFormat format =new SimpleDateFormat ("dd:MM:yyyy");
-		try{
 			Date date = format.parse(dateT);
 			setTime(date);
 
-		}catch(ParseException p){
-			return false;
-		}
-		return true;
 	}
 	public String toStringDate(){
 		DateFormat dateFormat = new SimpleDateFormat("dd:MM:yyyy");
@@ -33,27 +35,22 @@ public class DatePerso extends GregorianCalendar{
 		return dateFormat.format(this.getTime());
 	}
 	
-	public boolean setHeureDepart(String heureT) throws ParseException{
+	public void setHeureDepart(String heureT) throws ParseException{
 		SimpleDateFormat format =new SimpleDateFormat ("HH:mm");
-		try{
 		Date heure = format.parse(heureT);
 		setTime(heure);
-		}catch(ParseException p){
-			//System.err.println(p);
-			p.printStackTrace();
-			return false;
-		}
-		return true;
 	}
 	
 	public static void main(String[] args) throws ParseException{
 		
 		DatePerso d1=new DatePerso();
+		DatePerso d2=new DatePerso("07:03:1984");
 		d1.getTime();
 		System.out.println(d1.toStringDate());
 		System.out.println(d1.toStringHeure());
 		d1.setHeureDepart("05:05");
 		System.out.println(d1.toStringHeure());
+		System.out.println(d2.toStringDate());
 
 		System.out.println("test");
 		
